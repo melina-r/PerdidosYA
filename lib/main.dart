76822f10a494ac3547664d9a95de9cb7b304ac0c
melina-r <mretamozo@fi.uba.ar>
2024-12-01 11:32:09 -0300

    elimino cosas de notificaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:perdidos_ya/login.dart';
-import 'package:perdidos_ya/notifications.dart';
-import 'package:perdidos_ya/notifications/notifications.dart';
 import 'package:perdidos_ya/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  Notifications.initNotifications();
-  Notifications.saveToken();
-  initNotifications();
 
   runApp(const MyApp());
 }
